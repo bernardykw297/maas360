a45e1f0dd4f636067ba56dc5d70ccac5b998167c
Bernard Wong <bernard.wong@ibm.com>
2026-08-13 14:31:49 -0500

    Revert "Update 16 rep alignment records: Michael Shonde -> Marianne Johnston"
    This reverts commit 3ece0ddd255ed0878a60191c810f6166ddc8f765.

--- verify_fix.py
+++
@@ -1,7 +0,0 @@
-lines = open('maas-script.js', encoding='utf-8').readlines()
-remaining  = [i+1 for i,l in enumerate(lines) if 'Michael Shonde' in l]
-mar_as_rep = [i+1 for i,l in enumerate(lines) if '"Rep": "Marianne Johnston"' in l]
-print('Michael Shonde remaining:', len(remaining), '(should be 4 - Josh Knowles records)')
-print('Marianne Johnston as Rep:', len(mar_as_rep), '(should be 16)')
-for ln in remaining:
-    print(' line', ln, ':', lines[ln-1].strip())
